de3180f39d055b8e4adb89c4032e9c06a9adf862
Sobia Furqan <sobiafurqan@hotmail.com>
2021-06-15 10:35:27 +0100

    added JSONResponseService

--- Controllers/DishesController.js
+++ Controllers/DishesController.js
@@ -1,10 +1,13 @@
 const DbService = require('../Services/DbService')
 const DishesService = require('../Services/DishesService')
+const JSONResponseService = require('../Services/JSONResponseService')
 
 let getAllStarters = (req, res) => {
     DbService.connectToDb(async (db) => {
         let starters = await DishesService.getAllStarters(db, req)
-        let response = { 'property' : true, 'fruit' : starters }
+        let response = JSONResponseService.generateSuccessResponse()
+        response.message = "requested dishes successfully retrieved."
+        response.data = starters
         res.json(response)
     })
 }
