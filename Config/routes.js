2edcb4b70928494f3e9d980b56a6d8a7d38ed1c2
Sobia Furqan <sobiafurqan@hotmail.com>
2021-06-15 10:44:24 +0100

    Added mains, desserts, refreshments to DishesController

--- Config/routes.js
+++ Config/routes.js
@@ -3,9 +3,9 @@ const DishesController = require('../Controllers/DishesController')
 
 const routes = (app) => {
     app.get('/dishes/starters', DishesController.getAllStarters)
-    app.get('/dishes/mains', /* story 2 controller.function */)
-    app.get('/dishes/desserts', /* story 2 controller.function */ )
-    app.get('/dishes/refreshments', /* story 2 controller.function */ )
+    app.get('/dishes/mains', DishesController.getAllMains)
+    app.get('/dishes/desserts', DishesController.getAllDesserts)
+    app.get('/dishes/refreshments', DishesController.getAllRefreshments)
     app.post('/orders', /* story 3 controller.function */ )
     app.put('/orders/addToOrder', /* story 4 controller.function */ )
     app.delete('/orders', /* story 5 controller.function */ )
--- Controllers/DishesController.js
+++ Controllers/DishesController.js
@@ -6,10 +6,43 @@ let getAllStarters = (req, res) => {
     DbService.connectToDb(async (db) => {
         let starters = await DishesService.getAllStarters(db, req)
         let response = JSONResponseService.generateSuccessResponse()
-        response.message = "requested dishes successfully retrieved."
+        response.message = "Requested starters retrieved successfully."
         response.data = starters
         res.json(response)
     })
 }
 
+let getAllMains = (req, res) => {
+    DbService.connectToDb(async (db) => {
+        let mains = await DishesService.getAllMains(db, req)
+        let response = JSONResponseService.generateSuccessResponse()
+        response.message = "Requested mains retrieved successfully."
+        response.data = mains
+        res.json(response)
+    })
+}
+
+let getAllDesserts = (req, res) => {
+    DbService.connectToDb(async (db) => {
+        let desserts = await DishesService.getAllDesserts(db, req)
+        let response = JSONResponseService.generateSuccessResponse()
+        response.message = "Requested starters desserts successfully."
+        response.data = desserts
+        res.json(response)
+    })
+}
+
+let getAllRefreshments = (req, res) => {
+    DbService.connectToDb(async (db) => {
+        let refreshments = await DishesService.getAllRefreshments(db, req)
+        let response = JSONResponseService.generateSuccessResponse()
+        response.message = "Requested refreshments retrieved successfully."
+        response.data = refreshments
+        res.json(response)
+    })
+}
+
 module.exports.getAllStarters = getAllStarters
+module.exports.getAllMains = getAllMains
+module.exports.getAllDesserts = getAllDesserts
+module.exports.getAllRefreshments = getAllRefreshments
