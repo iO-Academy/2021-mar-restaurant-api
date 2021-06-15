98990ba084fa2d8a2020e895ee44995a25bf54ee
Nathaniel Payne <somersetnathaniel@gmail.com>
2021-06-15 12:00:10 +0100

    added try/catch to DishesController - getAllDishes and getAllAllDishesOfType

--- Controllers/DishesController.js
+++ Controllers/DishesController.js
@@ -4,21 +4,44 @@ const JSONResponseService = require('../Services/JSONResponseService')
 
 let getAllDishes = (req, res) => {
     DbService.connectToDb(async (db) => {
-        let dishes = await DishesService.getAllDishes(db, req)
-        let response = JSONResponseService.generateSuccessResponse()
-        response.message = "Requested starters retrieved successfully."
-        response.data = dishes
-        res.json(response)
+        try {
+            let dishes = await DishesService.getAllDishes(db, req)
+            let response = JSONResponseService.generateSuccessResponse()
+            response.message = "Requested starters retrieved successfully."
+            response.data = dishes
+            res.json(response)
+        } catch (e) {
+            let response = JSONResponseService.generateFailureResponse()
+            response.message = "The resources requested do not exist at the desired location."
+            res.json(response)
+        }
     })
 }
 
 let getAllDishesOfType = (req, res) => {
     DbService.connectToDb(async (db) => {
-        let dishes = await DishesService.getAllDishesOfType(db, req)
-        let response = JSONResponseService.generateSuccessResponse()
-        response.message = "Requested "  + req.params.course + " retrieved successfully."
-        response.data = dishes
-        res.json(response)
+        if (
+            req.params.course === 'starters' ||
+            req.params.course === 'mains' ||
+            req.params.course === 'desserts' ||
+            req.params.course === 'refreshments'
+        ) {
+            try {
+                let dishes = await DishesService.getAllDishesOfType(db, req)
+                let response = JSONResponseService.generateSuccessResponse()
+                response.message = "Requested " + req.params.course + " retrieved successfully."
+                response.data = dishes
+                res.json(response)
+            } catch (e) {
+                let response = JSONResponseService.generateFailureResponse()
+                response.message = "The resources requested do not exist at the desired location."
+                res.json(response)
+            }
+        } else {
+            let response = JSONResponseService.generateFailureResponse()
+            response.message = "The resources requested do not exist at the desired location."
+            res.json(response)
+        }
     })
 }
 
