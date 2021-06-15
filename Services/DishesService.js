9949d4e5eed9987dd4226ae7e45bcceea69ddf1a
Isabelle Cochrane <isabelle_cochrane@hotmail.co.uk>
2021-06-15 16:44:11 +0100

    dishes service

--- Services/DishesService.js
+++ Services/DishesService.js
@@ -1,5 +1,6 @@
 const ObjectId = require('mongodb').ObjectId
 
+
 let getAllDishes = async (db, req) => {
     const collection = db.collection('dishes')
     const data = await collection.find({}).toArray()
