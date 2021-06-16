5776d01b7bd5891a2216c55cd68db7e2de223404
Imy <imypullan@gmail.com>
2021-06-16 11:48:15 +0100

    timestamp added

--- Services/OrderService.js
+++ Services/OrderService.js
@@ -6,9 +6,10 @@ let createNewOrder = async (db, order) => {
 
 let submitFinalOrder = async  (db, order) => {
     const collection = db.collection('orders')
+    const timePlaced = new Date()
     const result = await collection.updateOne(
         { _id: order.orderId},
-        {$set: {isOrderSubmitted: true}}
+        {$set: {isOrderSubmitted: true, timePlaced: timePlaced}}
         )
     return result
 }
